896e435788149491640de991f7909a858d374416
Ashanka Randeniya <ashankarandeniya66@live.com>
2017-10-24 16:50:19 +0530

    2nd checkin

---
+++ ASRIdentity/ASRIdentity.Web/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASRIdentity.Web.CustomIdentity;
+using Microsoft.AspNetCore.Identity;
+
+namespace ASRIdentity.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        ////private readonly IMessageService _messageService;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+            ////this._messageService = messageService;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(string email, string password, string repassword)
+        {
+            if (password != repassword)
+            {
+                ModelState.AddModelError(string.Empty, "Password don't match");
+                return View();
+            }
+
+            var newUser = new ApplicationUser
+            {
+                UserName = email,
+                Email = email
+            };
+
+            var userCreationResult = await _userManager.CreateAsync(newUser, password);
+            if (!userCreationResult.Succeeded)
+            {
+                foreach (var error in userCreationResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return View();
+            }
+
+            var emailConfirmationToken = await _userManager.GenerateEmailConfirmationTokenAsync(newUser);
+            var tokenVerificationUrl = Url.Action("VerifyEmail", "Account", new { id = newUser.Id, token = emailConfirmationToken }, Request.Scheme);
+
+            ////await _messageService.Send(email, "Verify your email", $"Click <a href=\"{tokenVerificationUrl}\">here</a> to verify your email");
+
+            return this.Redirect("/");
+        }
+    }
+}
---
+++ ASRIdentity/ASRIdentity.Web/CustomIdentity/ApplicationDesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ASRIdentity.Web.CustomIdentity
+{
+    public class ApplicationDesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            var connectionString = configuration.GetConnectionString("AuthDbConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
